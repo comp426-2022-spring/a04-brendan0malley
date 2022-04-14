40c788528e265e0af77e69d5e9c8f3dd3d04f1ba
Brendan O'Malley <bromal23@live.unc.edu>
2022-04-14 15:11:40 -0400

    prepare database

--- database.js
+++ database.js
@@ -2,5 +2,10 @@
 
 const Database = require('better-sqlite3');
 
-// Connect to a database
+// Create/connect database
 const db = new Database('log.db')
+
+// Prepare database
+const stmt = db.prepare(`
+    SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
+    );
